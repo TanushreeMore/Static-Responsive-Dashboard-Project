20d7c2c1ffadc70310d93b6ff6f659b1e9cbadad
Tanushree <tmm181092@gmail.com>
2023-06-30 18:54:30 +0530

    Responsive Top Container ready

--- src/components/TopContainer.js
+++ src/components/TopContainer.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BiSearchAlt } from "react-icons/bi";
 import { FaBell, FaChevronDown } from "react-icons/fa";
+import Can from '../img/can_yaman3.jpg';
 
 function TopContainer() {
+     useEffect(() => {
+       const menuTarget = document.getElementById("menuChevron");
+       const menuContainer = document.getElementById("menuContainer");
+
+       menuTarget.addEventListener('mouseenter', ()=>{
+        menuTarget.style.transform = "rotate(180deg)";
+        menuContainer.style.transform = "translateX(0px)";
+       });
+       menuContainer.addEventListener('mouseleave', ()=>{
+        menuTarget.style.transform = "rotate(0deg)";
+        menuContainer.style.transform = "translateX(300px)";
+       });
+     }, [])
+     
+
   return (
     <div className='topContainer'>
         <div className="inputBox">
@@ -18,7 +34,7 @@ function TopContainer() {
             </i>
 
             <div className="profileImage">
-
+                <img src={Can} alt="Can Yaman" />
             </div>
 
             <p className="profileName">
@@ -27,6 +43,16 @@ function TopContainer() {
             <i className="menuChevron" id='menuChevron'>
                 <FaChevronDown/>
             </i>
+
+            <div className="menuContainer" id="menuContainer">
+                <ul>
+                    <li>Web Design</li>
+                    <li>UI /UX Design</li>
+                    <li>Form Design</li>
+                    <li>UI Design</li>
+                </ul>
+            </div>
+
         </div>
     </div>
   )
